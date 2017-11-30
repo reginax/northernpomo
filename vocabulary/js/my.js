3f5d91f176cf03b778166646397f47de1ebf7ae6
Regina Xu <rxu13@berkeley.edu>
2017-11-30 02:06:02 -0800

    Add sound files

--- vocabulary/js/my.js
+++ vocabulary/js/my.js
@@ -1,3 +1,57 @@
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
 $(document).ready(function() {
   var folder = "./sounds";
       $.ajax({
@@ -6,6 +60,7 @@ $(document).ready(function() {
             $(data).find("a").attr("href", function (i, val) {
                 if( val.match(/\.(wav|mp3)$/) ) {
                     $("body").append( "<img src='"+ folder + val +"'>" );
+
                 }
             });
         }
@@ -13,3 +68,47 @@ $(document).ready(function() {
 
 
 })
+function loadCategory(category) {
+    var myTableDiv = document.getElementById("metric_results")
+    var table = document.createElement('TABLE')
+    var tableBody = document.createElement('TBODY')
+
+    table.border = '1'
+    table.appendChild(tableBody);
+
+    var heading = new Array();
+    heading[0] = "Request Type"
+    heading[1] = "Group A"
+    heading[2] = "Groub B"
+    heading[3] = "Group C"
+    heading[4] = "Total"
+
+    var stock = new Array()
+    stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
+    stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
+    stock[2] = new Array("Colors", "88.625", "85.50", "85.81", "989")
+    stock[3] = new Array("Numbers", "88.625", "85.50", "85.81", "990")
+    stock[4] = new Array("Requests", "88.625", "85.50", "85.81", "991")
+
+    //TABLE COLUMNS
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    for (i = 0; i < heading.length; i++) {
+        var th = document.createElement('TH')
+        th.width = '75';
+        th.appendChild(document.createTextNode(heading[i]));
+        tr.appendChild(th);
+    }
+
+    //TABLE ROWS
+    for (i = 0; i < stock.length; i++) {
+        var tr = document.createElement('TR');
+        for (j = 0; j < stock[i].length; j++) {
+            var td = document.createElement('TD')
+            td.appendChild(document.createTextNode(stock[i][j]));
+            tr.appendChild(td)
+        }
+        tableBody.appendChild(tr);
+    }
+    myTableDiv.appendChild(table)
+}
