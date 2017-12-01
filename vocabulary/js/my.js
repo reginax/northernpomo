6b959f00b2f888c37fdf9168e56aa361dd513324
Regina Xu <rxu13@berkeley.edu>
2017-11-30 21:20:31 -0800

    Update

--- vocabulary/js/my.js
+++ vocabulary/js/my.js
@@ -1,123 +1,141 @@
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("myTable2");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
+function cellEntries(json, dest, divId) {
+    var table = document.createElement('table');
+        table.setAttribute("id", divId + "table");                 //Assign ID to <table> from the <div> name.
+    var thead = document.createElement('thead');
+    var tbody = document.createElement('tbody');
+    var thr;
+    var tr;
+    var entries = json.feed.entry;
+    var cols = json.feed.gs$colCount.$t;                                  //The number of columns in the sheet.
+    var link;                                                           //Teporary holder for the URL of a row.
+
+    for (var i=0; i <cols; i++) {                                       //For the first row of cells (column titles),
+        var entry = json.feed.entry[i];
+        if (entry.gs$cell.col == '1') {                                     //For First Column / URL Column, (1)
+            if (thr != null) {
+                tbody.appendChild(thr);
+            }
+            thr = document.createElement('tr');                                 //Create <thr>/<tr> (???).
         }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
+        else {                                                              //For all other columns,
+            var th = document.createElement('th');
+            th.appendChild(document.createTextNode(entry.content.$t));          //Create title for each column.
+            thr.appendChild(th);
         }
-      }
     }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-$(document).ready(function() {
-  var folder = "./sounds";
-      $.ajax({
-        url : folder,
-        success: function (data) {
-            $(data).find("a").attr("href", function (i, val) {
-
-                $(console.log(data));
-                if( val.match(/\.(wav|mp3)$/) ) {
-                    $("body").append( val);
-                }
-            });
+    for (var i=cols; i < json.feed.entry.length; i++) {                 //For all remaining cells,
+        var entry = json.feed.entry[i];
+        if (entry.gs$cell.col == '1') {                                     //For First Column / URL Column, (1)
+            if (tr != null) {
+                tbody.appendChild(tr);
+            }
+            tr = document.createElement('tr');                                  //Create <tr>.
+            hlink = entry.content.$t;                                           //Put URL content into hlink.
         }
-      });
-
-
-})
-function loadCategory(category) {
-    var myTableDiv = document.getElementById("metric_results")
-    var table = document.createElement('TABLE')
-    var tableBody = document.createElement('TBODY')
-
-    table.border = '1'
-    table.appendChild(tableBody);
-
-    var heading = new Array();
-    heading[0] = "Request Type"
-    heading[1] = "Group A"
-    heading[2] = "Groub B"
-    heading[3] = "Group C"
-    heading[4] = "Total"
-
-    var stock = new Array()
-    stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
-    stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
-    stock[2] = new Array("Colors", "88.625", "85.50", "85.81", "989")
-    stock[3] = new Array("Numbers", "88.625", "85.50", "85.81", "990")
-    stock[4] = new Array("Requests", "88.625", "85.50", "85.81", "991")
-
-    //TABLE ROWS
-    for (i = 0; i < stock.length; i++) {
-        var tr = document.createElement('TR');
-        var res = stock[i].split("_"); // eng_np_speaker
-
-        // Add eng
-        var td = document.createElement('TD');
-        td.appendChild(document.createTextNode(res[0]));
-        tr.append(td);
-
-        // Add NP
-        np = res[1].replace(/-/g, ' ');
-        var td = document.createElement('TD');
-        td.appendChild("<td class="click_sound" onclick="audio('o_weave_(wicker_basket)_EC.wav')">jaman</td>"
-        tr.append(td);
-
-        // Add speaker
-        var td = document.createElement('TD');
-        td.appendChild(document.createTextNode(res[-1]));
-        tr.append(td);
-
-        for (j = 0; j < stock[i].length; j++) {
-            var td = document.createElement('TD')
-            td.appendChild(document.createTextNode(stock[i][j]));
-            tr.appendChild(td)
+        else if (entry.gs$cell.col == '2') {                                //For Title Column,(2)
+            var td = document.createElement('td');
+            if (hlink != "None") {                                              //If there is a link,
+                var alink = document.createElement('a');                            //Make <a>
+                alink.appendChild(document.createTextNode(entry.content.$t));       //Put content in <a>
+                alink.setAttribute('href',hlink);                                   //Assign URL to <a>.
+                td.appendChild(alink);                                              //Put <a> in <td>.
+            }
+            else {                                                              //If there is no link,
+                td.appendChild(document.createTextNode(entry.content.$t));          //Put content in <td>.
+            }
+            tr.appendChild(td);
         }
-        tableBody.appendChild(tr);
+        else {                                                              //For all other columns,
+            var td = document.createElement('td');
+            if (entry.content.$t != "None") {                                   //If content is not "None",
+                td.appendChild(document.createTextNode(entry.content.$t));          //Output the content.
+            }
+            else {                                                              //Else,
+                td.appendChild(document.createTextNode(""));                        //Output a blank cell.
+            }
+            tr.appendChild(td);
+        }
     }
-    myTableDiv.appendChild(table)
-}
+    $(thead).append(thr);
+    $(tbody).append(tr);
+    $(table).append(thead);
+    $(table).append(tbody);
+    $(dest).append(table);
+    $(dest + "table").dataTable();
+
+};
+
+function importGSS(json){
+   var divId = "targetdivid"                            //ID of the target <div>.
+   cellEntries(json, "#" + divId, divId);
+};
+//
+// $(document).ready(function() {
+//   var folder = "./sounds";
+//       $.ajax({
+//         url : folder,
+//         success: function (data) {
+//             $(data).find("a").attr("href", function (i, val) {
+//
+//                 $(console.log(data));
+//                 if( val.match(/\.(wav|mp3)$/) ) {
+//                     $("body").append( val);
+//                 }
+//             });
+//         }
+//       });
+//
+//
+// })
+// function loadCategory(category) {
+//     var myTableDiv = document.getElementById("metric_results")
+//     var table = document.createElement('TABLE')
+//     var tableBody = document.createElement('TBODY')
+//
+//     table.border = '1'
+//     table.appendChild(tableBody);
+//
+//     var heading = new Array();
+//     heading[0] = "Request Type"
+//     heading[1] = "Group A"
+//     heading[2] = "Groub B"
+//     heading[3] = "Group C"
+//     heading[4] = "Total"
+//
+//     var stock = new Array()
+//     stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
+//     stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
+//     stock[2] = new Array("Colors", "88.625", "85.50", "85.81", "989")
+//     stock[3] = new Array("Numbers", "88.625", "85.50", "85.81", "990")
+//     stock[4] = new Array("Requests", "88.625", "85.50", "85.81", "991")
+//
+//     //TABLE ROWS
+//     for (i = 0; i < stock.length; i++) {
+//         var tr = document.createElement('TR');
+//         var res = stock[i].split("_"); // eng_np_speaker
+//
+//         // Add eng
+//         var td = document.createElement('TD');
+//         td.appendChild(document.createTextNode(res[0]));
+//         tr.append(td);
+//
+//         // Add NP
+//         np = res[1].replace(/-/g, ' ');
+//         var td = document.createElement('TD');
+//         td.appendChild("<td class="click_sound" onclick="audio('o_weave_(wicker_basket)_EC.wav')">jaman</td>"
+//         tr.append(td);
+//
+//         // Add speaker
+//         var td = document.createElement('TD');
+//         td.appendChild(document.createTextNode(res[-1]));
+//         tr.append(td);
+//
+//         for (j = 0; j < stock[i].length; j++) {
+//             var td = document.createElement('TD')
+//             td.appendChild(document.createTextNode(stock[i][j]));
+//             tr.appendChild(td)
+//         }
+//         tableBody.appendChild(tr);
+//     }
+//     myTableDiv.appendChild(table)
+// }
